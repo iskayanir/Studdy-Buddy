915d3b39b288cfb2d53dffc84937d3200c753f89
iska <106482132+iskayanir@users.noreply.github.com>
2024-06-23 13:34:54 +0000

    Updated server.js and package.json to use Express.js

--- server.js
+++ server.js
@@ -1,20 +1,40 @@
 // This is an example server.
 // To run it, right click server.js and select run.
 
-function handle(request, response) {
-  let path = request.path;
-  console.log("Received request: " + path);
-  if (path === '/') path = 'index.html';
-  getFile(path).subscribe(file => {
-    response.sendFile(file);
-  }, err => {
-    response.sendText('Page not found. Perhaps the filename is wrong?');
-  });
-}
+
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${port}`);
+});
+
+
+// function handle(request, response) {
+//   let path = request.path;
+//   console.log("Received request: " + path);
+//   if (path === '/') path = 'index.html';
+//   getFile(path).subscribe(file => {
+//     response.sendFile(file);
+//   }, err => {
+//     response.sendText('Page not found. Perhaps the filename is wrong?');
+//   });
+// }
 
 
 
-console.log('Server is running');
+// console.log('Server is running');
 
 
 
