e1315520121adba751d33730b2188235b3af1885
elad2424 <elfeldman8@gmail.com>
2024-07-18 06:51:38 +0000

    added finish button

--- public/js/search_page.js
+++ public/js/search_page.js
@@ -25,56 +25,60 @@ function filterItems() {
     });
         }
 
-        function filterFaculty(faculty) {
-            const gridItems = document.querySelectorAll('.grid-item');
+function filterFaculty(faculty) {
+    const gridItems = document.querySelectorAll('.grid-item');
 
-            gridItems.forEach(item => {
-                const facultyText = item.querySelector('.faculty').textContent;
+    gridItems.forEach(item => {
+        const facultyText = item.querySelector('.faculty').textContent;
 
-                if (facultyText.includes(faculty)) {
-                    item.style.display = 'block';
-                } else {
-                    item.style.display = 'none';
-                }
-            });
-        }
-
-
-        function filterAll(){
-            const gridItems = document.querySelectorAll('.grid-item');
-            
-            gridItems.forEach(item => {
+        if (facultyText.includes(faculty)) {
             item.style.display = 'block';
-            });
-      }
+        } else {
+            item.style.display = 'none';
+        }
+    });
+}
+
+
+function filterAll(){
+    const gridItems = document.querySelectorAll('.grid-item');
+    
+    gridItems.forEach(item => {
+    item.style.display = 'block';
+    });
+}
        
-      function fetchData() {
-                console.log("hello before");
-                fetch('https://study-buddy-d457d-default-rtdb.europe-west1.firebasedatabase.app/.json')
-                .then(response => response.json())
-                .then(data => {
-                  const gridContainer = document.getElementById('gridContainer');
-                  gridContainer.innerHTML = ''
-                                   
-                  Object.keys(data.courses).forEach(key => {
-                    const course = data.courses[key];
-            
-                    const gridItem = document.createElement('div');
-                    gridItem.classList.add('grid-item');
-            
-                    gridItem.innerHTML = `
-                      <h2 class="course-name">${course['Course Name']}</h2>
-                      <i class="bi bi-mortarboard icon"></i>
-                      <ul>
-                        <li class="faculty">פקולטה: ${course.Faculty}</li>
-                        <li class ="department"> חוג: ${course.Department}</li>
-                        <li class="teacher">מרצה: ${course['Lacture Name']}</li>
-                      </ul>
-                      <button class="add" onclick="addCourse()">הוסף קורס</button>
-                    `;
-            
-                    gridContainer.appendChild(gridItem);
-                  });
-                })
-                .catch(error => console.error('Error fetching data:', error));
-            }
+function fetchData() {
+    console.log("hello before");
+    fetch('https://study-buddy-d457d-default-rtdb.europe-west1.firebasedatabase.app/.json')
+    .then(response => response.json())
+    .then(data => {
+        const gridContainer = document.getElementById('gridContainer');
+        gridContainer.innerHTML = ''
+                        
+        Object.keys(data.courses).forEach(key => {
+        const course = data.courses[key];
+
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+
+        gridItem.innerHTML = `
+            <h2 class="course-name">${course['Course Name']}</h2>
+            <i class="bi bi-mortarboard icon"></i>
+            <ul>
+            <li class="faculty">פקולטה: ${course.Faculty}</li>
+            <li class ="department"> חוג: ${course.Department}</li>
+            <li class="teacher">מרצה: ${course['Lacture Name']}</li>
+            </ul>
+            <button class="add" onclick="addCourse()">הוסף קורס</button>
+        `;
+
+        gridContainer.appendChild(gridItem);
+        });
+    })
+    .catch(error => console.error('Error fetching data:', error));
+}
+
+function finish() {
+    
+}
